5cb34d74b631b27267e0603633447723d05eaea2
blakh1 <s.deleon.a@hotmail.com>
2024-04-09 20:53:50 -0300

    fixed modelsIndex

--- models/index.js
+++ models/index.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const router = express.Router();
-const userRoutes = require("./userRoutes");
-const productsRoutes = require("./productsRoutes");
-const orderRoutes = require("./orderRoutes")
+const { Sequelize } = require("sequelize");
+
+const User = require("./User");
+const Products = require("./Products"); 
+const Order = require("./Order");
+const Category = require("./Category");
 
 
-router.use("/user", userRoutes);
-router.use("/products", productsRoutes);
-router.use("/order", orderRoutes)
-router.use("/category", categoryRoutes)
 
-module.exports = router;
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    logging: false,
+  }
+);
+
+User.initModel(sequelize);
+Article.initModel(sequelize);
+
+User.hasMany(Article);
+Article.belongsTo(User);
+
+module.exports = {
+  sequelize,
+  User,
+  Category,
+};
