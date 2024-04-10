12bab74c8d8fbe1ec871b16f41faa793f42a03ce
blakh1 <s.deleon.a@hotmail.com>
2024-04-10 20:46:40 -0300

    FIX

--- models/index.js
+++ models/index.js
@@ -1,10 +1,22 @@
-const { sequelize, Model, DataTypes } = require("sequelize");
+/*const { sequelize, Model, DataTypes } = require("sequelize");
 
 const User = require("./User");
 const Products = require("./Products");
 const Order = require("./Order");
 const Category = require("./Category");
 const Admin = require("./Admin");
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    logging: false,
+  }
+);
 
 User.initModel(sequelize);
 Category.initModel(sequelize);
@@ -25,4 +37,44 @@ module.exports = {
   Category,
   Order,
   Admin,
+};*/
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const User = require("./User");
+const Products = require("./Products");
+const Order = require("./Order");
+const Category = require("./Category");
+const Admin = require("./Admin");
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    logging: false,
+  }
+);
+
+User.initModel(sequelize);
+Category.initModel(sequelize);
+Products.initModel(sequelize);
+Order.initModel(sequelize);
+Admin.initModel(sequelize);
+
+Category.hasMany(Products);
+Products.belongsTo(Category);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+module.exports = {
+  sequelize,
+  User,
+  Category,
+  Order,
+  Admin,
 };
+  
