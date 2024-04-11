89dcaa849ca05dfa74dc0ad26193dbb3cbebf70f
JoshNV08 <yadidjoshua@gmail.com>
2024-04-11 19:20:59 -0300

    Update Routes

---
+++ routes/productsRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { Products } = require("../models");
+const productsController = require("../controllers/productsController");
+
+router.get("/", productsController.index);
+router.get("/:id", productsController.show);
+router.post("/", productsController.store);
+router.patch("/:id", productsController.update);
+router.delete("/:id", productsController.destroy);
+
+module.exports = router;
