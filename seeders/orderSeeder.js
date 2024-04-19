8241c650ffa1f8b426e1b9f1a7e6eb5a0a279da0
JoshNV08 <yadidjoshua@gmail.com>
2024-04-19 18:19:35 -0300

    Update orderSeeder,js

--- seeders/orderSeeder.js
+++ seeders/orderSeeder.js
@@ -1,3 +1,16 @@
-const { Order } = require("../models")
+const { Order } = require("../models");
 
-    
+async function seedOrders() {
+    const orders = [];
+    for (let i = 0; i < 19; i++) {
+        const newOrder = {
+            items: 20,
+            status: 'pending',
+        };
+        orders.push(newOrder);
+    }
+    await Order.bulkCreate(orders);
+    console.log('¡Se crearon las órdenes!');
+}
+
+module.exports = seedOrders;
