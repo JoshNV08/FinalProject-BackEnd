632db383e8680b536212d7150a78d7eb59313fce
blakh1 <148918293+blakh1@users.noreply.github.com>
2024-06-04 19:12:34 -0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,14 +1,33 @@
-const {expressjwt: checkJwt} = require("express-jwt")
+const { expressjwt: checkJwt } = require("express-jwt");
 const express = require("express");
 const router = express.Router();
 const { Order } = require("../models/");
 const orderController = require("../controllers/orderController");
 
-router.get("/",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  orderController.index);
-router.get("/:id",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  orderController.show);
-router.post("/", checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  orderController.store);
-router.patch("/:id", checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  orderController.update);
-router.delete("/:id",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }), orderController.destroy);
-
+router.get(
+  "/",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  orderController.index
+);
+router.get(
+  "/:id",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  orderController.show
+);
+router.post(
+  "/",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  orderController.store
+);
+router.patch(
+  "/:id",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  orderController.update
+);
+router.delete(
+  "/:id",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  orderController.destroy
+);
 
 module.exports = router;
