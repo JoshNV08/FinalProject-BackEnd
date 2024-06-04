d0dfbe7fa9fcfc07bd58a72f3752e446db286c6f
blakh1 <148918293+blakh1@users.noreply.github.com>
2024-06-04 20:50:29 -0300

    fetch the products

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,14 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { Product } = require("../models/");
 const productsController = require("../controllers/productsController");
-const {expressjwt: checkJwt} = require("express-jwt");
+const { expressjwt: checkJwt } = require("express-jwt");
 
-
-
-router.get("/",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  productsController.index);
-router.get("/:id",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  productsController.show);
-router.post("/", checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  productsController.store);
-router.patch("/:id", checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),  productsController.update);
-router.delete("/:id",checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }), productsController.destroy);
+router.get("/", productsController.index);
+router.get("/:id", productsController.show);
+router.post(
+  "/",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  productsController.store
+);
+router.patch(
+  "/:id",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  productsController.update
+);
+router.delete(
+  "/:id",
+  checkJwt({ secret: "UnStringMuySecreto", algorithms: ["HS256"] }),
+  productsController.destroy
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(port, () =>
-  console.log(`Servidor corriendo en http://${process.env.APP_DOMAIN}.\n`)
+  console.log(
+    `Servidor corriendo en http://${
+      process.env.APP_DOMAIN || "localhost"
+    }:${port}.\n`
+  )
 );
