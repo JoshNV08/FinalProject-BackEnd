a96c4b1d30b46e32dffa3be85066c7242f1aa22e
JoshNV08 <yadidjoshua@gmail.com>
2024-06-28 20:06:31 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,8 @@ router.get("/users", jwtMiddleware, userController.index);
 router.get("/users/:id", jwtMiddleware, userController.show);
 router.put("/users/:id", jwtMiddleware, userController.update);
 router.delete("/users/:id", jwtMiddleware, userController.destroy);
+router.put("/user/profile/:id", jwtMiddleware, userController.updateProfile);
+router.get("/user/profile/:id", jwtMiddleware, userController.getProfile);
 
 // Rutas públicas
 router.post("/users", userController.store);
