4c3d489b857fab8278db7f8e91d81ff4601b08ea
Joshua Nuñez <yadidjoshua@gmail.com>
2024-07-04 07:30:59 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,15 +8,13 @@ const jwtMiddleware = checkJwt({
   algorithms: ["HS256"],
 });
 
-// Rutas protegidas con JWT
-router.get("/users", jwtMiddleware, userController.index);
-router.get("/users/:id", jwtMiddleware, userController.show);
-router.put("/users/:id", jwtMiddleware, userController.update);
-router.delete("/users/:id", jwtMiddleware, userController.destroy);
-router.put("/user/profile/:id", jwtMiddleware, userController.update);
-router.get("/user/profile/:id", jwtMiddleware, userController.show);
+router.get("/", jwtMiddleware, userController.index);
+router.get("/:id", jwtMiddleware, userController.show);
+router.put("/:id", jwtMiddleware, userController.update);
+router.delete("/:id", jwtMiddleware, userController.destroy);
+router.put("/profile/:id", jwtMiddleware, userController.update);
+router.get("/profile/:id", jwtMiddleware, userController.show);
 
-// Rutas públicas
-router.post("/users", userController.store);
+router.post("/", userController.store);
 
 module.exports = router;
